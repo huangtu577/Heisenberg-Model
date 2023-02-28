3767f630f7bb731752ec7d9e50d8aab1affc9188
Fabian <fabiprandl@gmail.com>
2023-02-28 14:10:52 +0100

    Updated main for production

--- main.cpp
+++ main.cpp
@@ -76,35 +76,66 @@ void mainloop(double J, double beta, const int N, size_t L,
 int main() {
   double J = 1.0;
   double beta = 0.5;
-  const int N = 1e4;
+  const int N = 5e6;
   size_t L = 11;
   const std::string FILENAME = "heisenberg_model_test_new.h5";
   std::cout << std::setprecision(10);
   const size_t multihitparam = 7;
-  size_t steps = 12;
+  size_t steps = 16;
+  const size_t CORES = 16;
+
+  double start_value = 0.55;
+  double increment = 0.02;
+
+  for (size_t i = 0; i < steps; i++) {
+    std::cout << start_value + i * increment << std::endl;
+  }
+  
+
+  if(steps % CORES != 0){
+    throw std::runtime_error("For Batches larger than N, use a number, that is divisible by 12.");
+  }
 
   {
     H5Easy::File file(FILENAME, H5Easy::File::ReadWrite | H5Easy::File::Create);
   }
   //   std::cout << grid.snapshot() << std::endl;
 
+  // create a list of L values containing 25, 30, 35, 40, 45, 50, 55, 60
+  std::vector<size_t> L_list = {15, 25, 30, 35, 40};
 
-#pragma omp parallel for
-  for (int i = 0; i < steps; i++) {
+  // create a list of beta values containing 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  // std::vector<double> beta_list =
 
-    if(steps > 12 && steps % 12 != 0){
-    throw std::runtime_error("For Batches larger than N, use a number, that is divisible by 12.");
+  for(size_t L: L_list){
+    std::cout << "Starting L = " << L << std::endl;
+    
+    
+    #pragma omp parallel for
+      for (int i = 0; i < steps; i++) {
+
+        double beta = start_value + increment *i;
+
+        Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
+        grid.mainloop();
+        // sleep for 1 sec
+
+      
+      }
+      std::cout << "Finished L = " << L << std::endl;
   }
+  return 0;
 
-
-    double beta = 0.60 + 0.03*i;
-
-    Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
-    grid.mainloop();
-    // sleep for 1 sec
-
-   
-  }
 
   // // std::cout << grid << std::endl;
   // std::cout << grid.calculate_magnetization() << std::endl;
