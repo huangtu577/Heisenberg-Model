937f611c98e9063688cac075d7616df73a555c2b
Fabian <fabiprandl@gmail.com>
2023-03-01 10:19:43 +0100

    Setup Script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 
 # Installing the main dependencies 
 apt install cmake ninja-build build-essential 
-apt-get install libboost-all-dev libomp-dev
+apt-get install libboost-all-dev libomp-dev libhdf5-serial-dev
 
-# Installing the Code
-git clone --branch production https://github.com/huangtu577/Heisenberg-Model.git --recurse-submodules
