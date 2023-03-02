8f3cf96727d4eb8f8273b9842f10367b387ec212
Fabian <fabiprandl@gmail.com>
2023-03-02 14:07:19 +0100

    Clean Up

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ void mainloop(double J, double beta, const int N, size_t L,
               std::string const FILENAME, const size_t multihitparam) {
   std::cout << "beta: " << beta << std::endl;
 
-  
   Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
   // std::cout << grid << std::endl;
 
@@ -75,86 +74,43 @@ void mainloop(double J, double beta, const int N, size_t L,
 
 int main() {
   double J = 1.0;
-  double beta = 0.5;
-  const int N = 5e6;
+  double beta = 0.7;
+  const int N = 1e5;
   size_t L = 11;
-  const std::string FILENAME = "heisenberg_model.h5";
+  const std::string FILENAME = "heisenberg_model_test.h5";
   std::cout << std::setprecision(10);
   const size_t multihitparam = 7;
-  size_t steps = 16;
-  const size_t CORES = 16;
+  size_t steps = 12;
+  const size_t CORES = 12;
 
   double start_value = 0.55;
   double increment = 0.02;
 
-  for (size_t i = 0; i < steps; i++) {
-    std::cout << start_value + i * increment << std::endl;
-  }
-  
-
-  if(steps % CORES != 0){
-    throw std::runtime_error("For Batches larger than N, use a number, that is divisible by 12.");
-  }
-
   {
     H5Easy::File file(FILENAME, H5Easy::File::ReadWrite | H5Easy::File::Create);
   }
+  // Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
+  // grid.mainloop(true);
+  // return 0;
+
   //   std::cout << grid.snapshot() << std::endl;
 
   // create a list of L values containing 25, 30, 35, 40, 45, 50, 55, 60
-  std::vector<size_t> L_list = {15, 25, 30, 35, 40};
+  // std::vector<size_t> L_list = {15, 25, 30, 35, 40};
+  std::vector<size_t> L_list = {15};
 
-  // create a list of beta values containing 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  // std::vector<double> beta_list =
-
-  for(size_t L: L_list){
+  for (size_t L : L_list) {
     std::cout << "Starting L = " << L << std::endl;
-    
-    
-    #pragma omp parallel for
-      for (int i = 0; i < steps; i++) {
 
-        double beta = start_value + increment *i;
+#pragma omp parallel for
+    for (int i = 0; i < steps; i++) {
 
-        Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
-        grid.mainloop();
-        // sleep for 1 sec
+      double beta = start_value + increment * i;
 
-      
-      }
-      std::cout << "Finished L = " << L << std::endl;
+      Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
+      grid.mainloop(true);
+    }
+    std::cout << "Finished L = " << L << std::endl;
   }
-  return 0;
-
-
-  // // std::cout << grid << std::endl;
-  // std::cout << grid.calculate_magnetization() << std::endl;
-  // // open fstream
-  // std::ofstream mag_file;
-  // std::ofstream energy_file;
-  // mag_file.open("mag.dat");
-  // energy_file.open("energy.dat");
-  // for (int i = 0; i < N; i++) {
-  //     grid.hb_sweep();
-  //     mag_file << grid.calculate_magnetization() << "\n";
-  //     energy_file << grid.calculate_energy() << "\n";
-
-  // }
-
-  // mag_file.close();
-  // energy_file.close();
-
-  //   mainloop(J, beta, N, L, FILENAME);
-
   return 0;
 }
