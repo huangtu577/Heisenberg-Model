2e90892303da56b87c422d239e50d5d6c2a07c8d
Fabian <fabiprandl@gmail.com>
2023-03-10 09:19:46 +0100

    Better Mainloop,

--- main.cpp
+++ main.cpp
@@ -3,28 +3,25 @@
 #include "highfive/H5Exception.hpp"
 #include <array>
 #include <boost/multi_array.hpp>
+#include <csignal>
 #include <exception>
 #include <fstream>
 #include <highfive/H5Easy.hpp>
 #include <highfive/H5File.hpp>
 #include <iomanip>
 #include <iostream>
+#include <map>
 // #include <omp.h>
 #include <stdexcept>
 #include <string>
 #include <unistd.h>
+#include <utility>
 #include <vector>
-#include <csignal>
-
-
 
 void signalHandler(int signum) {
   std::cout << "Interrupt signal (" << signum << ") received.\n";
   globals::Stop = true;
-
-  
 }
-
 
 int main() {
   double J = 1.0;
@@ -37,75 +34,59 @@ int main() {
   const size_t INSTANCE_NUM{0};
   const size_t INSTANCES{3};
 
-
   // std::vector<Grid3D*> grids;
 
-  /* TODO: Need to make a smaller max value and finer steps around the critical temperatur*/
-  double increment = 0.021/3;
+  /* TODO: Need to make a smaller max value and finer steps around the critical
+   * temperatur*/
+  double increment = 0.021 / 3;
   double start_value = 0.58;
 
-  if(start_value +  2 * increment * INSTANCE_NUM * (steps + 1)> 0.75){
-    throw std::runtime_error("Start value is too high");
-  }
-
-  if(INSTANCE_NUM >= INSTANCES){
+  if (INSTANCE_NUM >= INSTANCES) {
     throw std::runtime_error("Instance number is too high");
   }
 
   std::signal(SIGTERM, signalHandler);
 
-  std::cout << "Lowest beta: " << start_value << "Highest beta: " << start_value + increment * steps << std::endl;
-  
+  std::cout << "Lowest beta: " << start_value
+            << "Highest beta: " << start_value + increment * steps << std::endl;
 
-  if(steps % CORES != 0){
-    throw std::runtime_error("For Batches larger than N, use a number, that is divisible by " + std::to_string(CORES));
+  if (steps % CORES != 0) {
+    throw std::runtime_error(
+        "For Batches larger than N, use a number, that is divisible by " +
+        std::to_string(CORES));
   }
 
   {
     H5Easy::File file(FILENAME, H5Easy::File::ReadWrite | H5Easy::File::Create);
   }
-  //   std::cout << grid.snapshot() << std::endl;
 
-  // create a list of L values containing 25, 30, 35, 40, 45, 50, 55, 60
-  std::vector<size_t> L_list = {15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 17, 23, 27, 33, 37, 43};
-  // std::vector<size_t> L_list = {45, 50, 55, 60, 17, 23, 27, 33, 37, 43};
-  
+  std::vector<size_t> L_list = {25, 30, 35, 40, 45, 50,
+                                55, 60, 17, 23, 27, 33, 37, 43};
 
-  for(size_t L: L_list){
-    //Print out the time
-    time_t now = time(0);
-    char* dt = ctime(&now);
-    std::cout << "Start: Local date and time is: " << dt << std::endl;
-    
-    std::cout << "Starting L = " << L << std::endl;
-
-    #pragma omp parallel for
-      for (int i = 0; i < steps; i++) {
-
-        
-
-        
-        
-
-        // double beta = start_value + increment *i;
-
-        double beta = start_value + increment*i*INSTANCES + increment*INSTANCE_NUM;
-        std::cout << "Beta: " << beta << std::endl;
-        
-
-        
-        Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
-
-
-        grid.mainloop(true);
-        
-
-      
-      }
-      std::cout << "Finished L = " << L << std::endl;
-      now = time(0);
-      dt = ctime(&now);
-      std::cout << "End: Local date and time is: " << dt << std::endl;
+  std::vector<std::pair<size_t, double>> L_list_beta;
+  for (size_t L : L_list) {
+    for (int i = 0; i < steps; i++) {
+      double beta =
+          start_value + increment * i * INSTANCES + increment * INSTANCE_NUM;
+      L_list_beta.push_back(std::make_pair(L, beta));
+    }
   }
+
+#pragma omp parallel for schedule(static, 1)
+  for (auto &[L, beta] : L_list_beta) {
+    time_t now = time(0);
+    char *dt = ctime(&now);
+    std::cout << "Start: Local date and time is: " << dt
+              << "\nStarting L = " << L << "Beta= " << beta << std::endl;
+
+    Grid3D grid(L, L, L, J, beta, multihitparam, N, FILENAME);
+    grid.mainloop(true);
+
+    now = time(0);
+    dt = ctime(&now);
+    std::cout << "Finished L = " << L << "Beta= " << beta
+              << "\nEnd: Local date and time is: " << dt << std::endl;
+  }
+
   return 0;
 }
